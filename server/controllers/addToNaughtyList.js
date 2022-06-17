de0228a6cb73b65fb5dba971d8288c7bf0e033f2
Vince <vincent.tran.ece@gmail.com>
2022-06-17 01:01:46 -0500

    adds functionality to clear forum on submit

--- server/controllers/addToNaughtyList.js
+++ server/controllers/addToNaughtyList.js
@@ -3,12 +3,10 @@ const { People } = require('../models');
 async function addToNaughtyList(name, description, naughtiness) {
 
   let exists = await People.exists({name: name});
-  console.log('line 6', exists);
   //check if name already exists in database
   if(exists) {
     let error = new Error('The name already exists in the database!');
     error.statusCode = 409;
-    console.log('line 10:', error);
     throw error;
   } else {
     return People.create({name: name, description: description, naughtiness: naughtiness});
